a3d8aa0d3c71cb0663e6fb957328a066bd3016f0
tobi <c4rgon4t@gmail.com>
2019-12-05 13:57:34 +0100

    Modified Day 1 code to be more easily generalized to Day 2

--- 2019/day1.py
+++ 2019/day1.py
@@ -11,10 +11,15 @@ if comp_mass_strings[-1] == "":
 # parse the strings to get integers
 comp_masses = [int(string) for string in comp_mass_strings]
 
-# for each component, get the fuel requirement and add it to the total
+
+# for each component, we need the fuel requirement of a single component
+def get_comp_fuel_requirement(mass):
+    return mass // 3 - 2
+
+
+# now sum all the fuel requirements
 total_fuel_required = 0
 for mass in comp_masses:
-    comp_fuel_required = mass // 3 - 2
-    total_fuel_required += comp_fuel_required
+    total_fuel_required += get_comp_fuel_requirement(mass)
 
 print(total_fuel_required)
