466266d20ab07bce666adf8a3bc8710fa8c15276
Tobi <c4rgon4t@gmail.com>
2020-12-02 01:15:06 +0100

    day 1 part 2

--- 2020/day1.go
+++ 2020/day1.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	input := readAndSortInput("2020\\day1_input")
 	twoNumbers(2020, input)
+	threeNumbers(2020, input)
 }
 
 func twoNumbers(n int, input []int) {
@@ -57,6 +58,52 @@ func twoNumbers(n int, input []int) {
 		lastTooSmall = nowTooSmall
 	}
 	fmt.Printf("No pair summing to %d found in %d tries.", n, tries)
+}
+
+func threeNumbers(n int, input []int) {
+	// Same approach as in the two number case, but now with a 3D-table and a plane of color transition.
+	// Search one layer similar to two number case, then go down one layer. Keep x < y < z
+	// The runtime including sorting for input size m is at most O(m) * T * S where T is the runtime of the two number
+	// case and S is the time the sorting takes, so O(m^2 log m). It might be even lower.
+
+	x := 0
+	y := 1
+	z := 2
+
+	lastTooSmall := true
+	search := true
+
+	tries := 0
+	for x < y && y < z && z < len(input) {
+		for y < z && z < len(input) {
+			tries++
+
+			sum := input[x] + input[y] + input[z]
+			nowTooSmall := sum < n
+
+			switch {
+			case sum == n:
+				fmt.Printf("\nFound triple summing to %d in %d tries: %d, %d, %d with product %d",
+					n, tries, input[x], input[y], input[z], input[x]*input[y]*input[z])
+				return
+			case !search && lastTooSmall != nowTooSmall:
+				y++
+				search = true
+			case nowTooSmall:
+				z++
+				search = false
+			case !nowTooSmall:
+				z--
+				search = false
+			}
+
+			lastTooSmall = nowTooSmall
+		}
+		x++
+		y, z = x+1, x+2
+		search = true
+	}
+	fmt.Printf("No triple summing to %d found in %d tries.", n, tries)
 }
 
 func readAndSortInput(path string) (result []int) {
