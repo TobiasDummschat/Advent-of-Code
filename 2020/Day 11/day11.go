8666bb7b5bc5fabc7fd69e6e57a22de224c99176
Tobi <c4rgon4t@gmail.com>
2020-12-11 17:13:32 +0100

    Day 11 - Part 1

--- 2020/Day 11/day11.go
+++ 2020/Day 11/day11.go
@@ -1,34 +1,86 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
 	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 )
 
 type Position string
 
 const (
-	Floor    Position = "."
-	Empty    Position = "L"
-	Occupied Position = "#"
+	Floor        Position = "."
+	EmptySeat    Position = "L"
+	OccupiedSeat Position = "#"
 )
 
 type Area [][]Position
 
-func main() {
-	parseInput("2020\\Day 11\\day11_input")
+func (thisArea Area) equals(area Area) bool {
+	if len(thisArea) != len(area) {
+		return false
+	}
+	for i := 0; i < len(thisArea); i++ {
+		if len(thisArea[i]) != len(area[i]) {
+			return false
+		}
+		for j := 0; j < len(thisArea[i]); j++ {
+			if thisArea[i][j] != area[i][j] {
+				return false
+			}
+		}
+	}
+	return true
 }
 
-func doIteration(oldArea Area) Area {
+func main() {
+	area := parseInput("2020\\Day 11\\day11_input")
+	part1Areas := iterateUntilConstant(area)
+
+	writeGifToFile(areasToGIF(part1Areas), "2020\\Day 11\\part1.gif")
+}
+
+func iterateUntilConstant(area Area) (areas []Area) {
+	iterations := 0
+	areas = append(areas, area)
+
+	oldArea, newArea := area, iterate(area)
+	for !oldArea.equals(newArea) {
+		iterations++
+		areas = append(areas, newArea)
+		oldArea, newArea = newArea, iterate(newArea)
+	}
+
+	fmt.Printf("Area constant after %d iterations with %d occupied seats in state:\n%v",
+		iterations, countAllOccupied(oldArea), oldArea)
+	return areas
+}
+
+func writeGifToFile(areaGif *gif.GIF, path string) {
+	file, err := os.Create(path)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		gif.EncodeAll(file, areaGif)
+		file.Close()
+	}
+}
+
+func iterate(oldArea Area) Area {
 	newArea := make([][]Position, len(oldArea))
 	for i, row := range oldArea {
 		newArea[i] = make([]Position, len(row))
 		for j, pos := range row {
 			count := nearbyOccupied(oldArea, i, j)
-			if pos == Empty && count == 0 {
-				newArea[i][j] = Occupied
-			} else if pos == Occupied && count >= 4 {
-				newArea[i][j] = Empty
+			if pos == EmptySeat && count == 0 {
+				newArea[i][j] = OccupiedSeat
+			} else if pos == OccupiedSeat && count >= 4 {
+				newArea[i][j] = EmptySeat
 			} else {
 				newArea[i][j] = oldArea[i][j]
 			}
@@ -42,9 +94,20 @@ func nearbyOccupied(area Area, row, col int) (count int) {
 		for j := col - 1; j <= col+1; j++ {
 			if i == row && j == col {
 				continue
-			} else if 0 < i || 0 < j || len(area) <= i || len(area[i]) <= j {
+			} else if i < 0 || j < 0 || i >= len(area) || j >= len(area[i]) {
 				continue
-			} else if area[i][j] == Occupied {
+			} else if area[i][j] == OccupiedSeat {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func countAllOccupied(area Area) (count int) {
+	for i := range area {
+		for _, pos := range area[i] {
+			if pos == OccupiedSeat {
 				count++
 			}
 		}
@@ -59,9 +122,65 @@ func parseInput(path string) Area {
 	for i, row := range rows {
 		area[i] = make([]Position, len(row))
 		positions := strings.Split(row, "")
-		for j, pos := range positions {
-			area[i][j] = Position(pos)
+		for j, strPos := range positions {
+			pos := Position(strPos)
+			if pos != Floor && pos != EmptySeat && pos != OccupiedSeat {
+				log.Panicf("Found position not valid: %s", strPos)
+			}
+			area[i][j] = pos
 		}
 	}
 	return area
+}
+
+//goland:noinspection GoNilness
+func areasToGIF(areas []Area) *gif.GIF {
+	if len(areas) == 0 {
+		log.Panic("Cannot convert empty area slice to gif.")
+	}
+
+	var images []*image.Paletted
+	var delays []int
+	delay := 10
+
+	for _, area := range areas {
+		img := areaToImage(area)
+		images = append(images, img)
+		delays = append(delays, delay)
+	}
+
+	delays[0] = delay * 10
+	delays[len(delays)-1] = delay * 10
+
+	return &gif.GIF{
+		Image: images,
+		Delay: delays,
+	}
+}
+
+func areaToImage(area Area) *image.Paletted {
+	white := color.RGBA{R: 0xff, G: 0xff, B: 0xff, A: 0xff}
+	gray := color.RGBA{R: 0x44, G: 0x44, B: 0x44, A: 0xff}
+	black := color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xff}
+
+	palette := color.Palette{white, gray, black}
+	height := len(area)
+	width := len(area[0])
+	img := image.NewPaletted(image.Rect(0, 0, height, width), palette)
+
+	for x := range area {
+		for y, pos := range area[x] {
+			if pos == Floor {
+				img.Set(x, y, white)
+			} else if pos == EmptySeat {
+				img.Set(x, y, gray)
+			} else if pos == OccupiedSeat {
+				img.Set(x, y, black)
+			} else {
+				log.Printf("Unkown type of position: %s", pos)
+			}
+		}
+	}
+
+	return img
 }
