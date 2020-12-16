7a0a6b67669cc43b675aa8c7e71991e4ebb0c956
Tobi <c4rgon4t@gmail.com>
2020-12-16 16:32:24 +0100

    Day 16 - Part 2

--- 2020/Day 16/day16.go
+++ 2020/Day 16/day16.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"strconv"
 	"strings"
 )
@@ -18,6 +19,7 @@ func (interval Interval) contains(n int) bool {
 type Rule struct {
 	name      string
 	intervals []Interval
+	index     int
 }
 
 func (rule Rule) allows(n int) bool {
@@ -48,19 +50,85 @@ func (ticket Ticket) invalidValues(rules []Rule) (result []int) {
 }
 
 func main() {
-	rules, _, nearbyTickets := parseInput("2020\\Day 16\\day16_input")
-	scanningErrorRate(rules, nearbyTickets)
+	rules, myTicket, nearbyTickets := parseInput("2020\\Day 16\\day16_input")
+	slightlyValidTickets, _ := discardCompletelyInvalidTickets(rules, nearbyTickets)
+
+	allSlightlyValidTickets := append(slightlyValidTickets, myTicket)
+	rulesPerIndex := matchRulesToIndices(rules, allSlightlyValidTickets)
+
+	product := 1
+	for i := 0; i < len(rules); i++ {
+		rule := rulesPerIndex[i]
+		if hasPrefix(rule.name, "departure") {
+			product *= myTicket[i]
+		}
+	}
+	fmt.Printf("The product of the fields on my ticket starting with \"contains\" is %d.\n", product)
 }
 
-func scanningErrorRate(rules []Rule, nearbyTickets []Ticket) (errorRate int) {
+func discardCompletelyInvalidTickets(rules []Rule, nearbyTickets []Ticket) (slightlyValidTickets []Ticket, errorRate int) {
 	for _, ticket := range nearbyTickets {
 		invalidValues := ticket.invalidValues(rules)
+		if len(invalidValues) == 0 {
+			slightlyValidTickets = append(slightlyValidTickets, ticket)
+		}
 		for _, val := range invalidValues {
 			errorRate += val
 		}
 	}
-	fmt.Printf("Nearby ticket scanning error rate: %d\n", errorRate)
-	return errorRate
+	fmt.Printf("%d tickets out of %d are not completely invalid. Nearby ticket scanning error rate: %d\n",
+		len(slightlyValidTickets), len(nearbyTickets), errorRate)
+	return slightlyValidTickets, errorRate
+}
+
+func matchRulesToIndices(rules []Rule, tickets []Ticket) (rulesPerIndex map[int]Rule) {
+	possibleRulesPerIndex := possibleRulesPerIndex(rules, tickets)
+
+	rulesPerIndex = make(map[int]Rule)
+
+	for len(rulesPerIndex) < len(rules) {
+		ticketLength := len(tickets[0])
+		for i := 0; i < ticketLength; i++ {
+			if rulesPerIndex[i].name != "" {
+				continue
+			}
+			possible := possibleRulesPerIndex[i]
+			if len(possible) == 1 {
+				ruleIndex := possible[0]
+				rule := rules[ruleIndex]
+				rulesPerIndex[i] = rule
+				rule.index = i
+				for j := 0; j < ticketLength; j++ {
+					if i != j {
+						possibleRulesPerIndex[j] = removeElement(possibleRulesPerIndex[j], ruleIndex)
+					}
+				}
+				break
+			} else if len(possible) == 0 {
+				log.Panicf("No possible rules for index %d!", i)
+			}
+		}
+	}
+	return rulesPerIndex
+}
+
+func possibleRulesPerIndex(rules []Rule, tickets []Ticket) (possibleRulesPerIndex map[int][]int) {
+	possibleRulesPerIndex = make(map[int][]int)
+	for i := 0; i < len(tickets[0]); i++ {
+		for j, rule := range rules {
+			ok := true
+			for _, ticket := range tickets {
+				if !rule.allows(ticket[i]) {
+					ok = false
+					break
+				}
+			}
+			if ok {
+				possibleRulesPerIndex[i] = append(possibleRulesPerIndex[i], j)
+			}
+		}
+	}
+	return possibleRulesPerIndex
 }
 
 func parseInput(path string) (rules []Rule, myTicket Ticket, nearbyTickets []Ticket) {
@@ -105,4 +173,22 @@ func parseRule(ruleString string) (rule Rule) {
 	}
 
 	return rule
+}
+
+func removeElement(slice []int, n int) []int {
+	for i := 0; i < len(slice); i++ {
+		if slice[i] == n {
+			return append(slice[:i], slice[i+1:]...)
+		}
+	}
+	return slice
+}
+
+func hasPrefix(s, prefix string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[:i+1] == prefix {
+			return true
+		}
+	}
+	return false
 }
