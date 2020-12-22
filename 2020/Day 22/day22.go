744865739e87c2c5c5bbd006328a81f312bcd660
Tobi <c4rgon4t@gmail.com>
2020-12-22 10:15:10 +0100

    Day 22 - add game log

--- 2020/Day 22/day22.go
+++ 2020/Day 22/day22.go
@@ -43,16 +43,32 @@ func (memory Memory) happenedBefore(deck1, deck2 Deck) bool {
 	return false
 }
 
+type Counter struct {
+	round, game, highestGame int
+}
+
+func newCounter() Counter {
+	return Counter{1, 1, 1}
+}
+
 func main() {
 	deck1, deck2 := parseInput("2020\\Day 22\\day22_input")
-	winner, winningScore := combatGame(deck1, deck2, false)
+
+	counter := newCounter()
+	winner, winningScore := combatGame(deck1, deck2, false, false, counter)
 	fmt.Printf("Player %d wins the game of Combat with a score of %d.\n", winner, winningScore)
-	winner, winningScore = combatGame(deck1, deck2, true)
+
+	counter = newCounter()
+	winner, winningScore = combatGame(deck1, deck2, true, false, counter)
 	fmt.Printf("Player %d wins the game of Recursive Combat with a score of %d.\n", winner, winningScore)
 }
 
-func combatGame(deck1, deck2 Deck, recursive bool) (winner, winningScore int) {
+func combatGame(deck1, deck2 Deck, recursive, log bool, counter Counter) (winner, winningScore int) {
 	memory := Memory{}
+
+	if recursive && log {
+		fmt.Printf("=== Game %d ===\n\n")
+	}
 
 	// play the game until it is over
 	for len(deck1) > 0 && len(deck2) > 0 {
@@ -65,20 +81,43 @@ func combatGame(deck1, deck2 Deck, recursive bool) (winner, winningScore int) {
 		}
 
 		// play a round
-		combatRound(&deck1, &deck2, recursive)
+		combatRound(&deck1, &deck2, recursive, log, &counter)
+		counter.round++
 	}
 
 	// game is over
 	if len(deck1) == 0 { // player 2 wins
+		if log {
+			fmt.Printf("The winner of game %d is player 2!\n\n", counter.game)
+		}
 		return 2, score(deck2)
 	} else { // player 1 wins
+		if log {
+			fmt.Printf("The winner of game %d is player 1!\n\n", counter.game)
+		}
 		return 1, score(deck1)
 	}
 }
 
-func combatRound(deck1, deck2 *Deck, recursive bool) {
+func combatRound(deck1, deck2 *Deck, recursive, log bool, counter *Counter) {
+	if log {
+		if recursive {
+			fmt.Printf("-- Round %d (Game %d) --\n", counter.round, counter.game)
+		} else {
+			fmt.Printf("-- Round %d --\n", counter.round)
+		}
+		fmt.Printf("Player 1's deck: %v\n", *deck1)
+		fmt.Printf("Player 2's deck: %v\n", *deck2)
+	}
+
 	card1 := deck1.draw()
 	card2 := deck2.draw()
+
+	if log {
+		fmt.Printf("Player 1 plays: %d\n", card1)
+		fmt.Printf("Player 2 plays: %d\n", card2)
+	}
+
 	winner := -1
 
 	if recursive && card1 <= len(*deck1) && card2 <= len(*deck2) {
@@ -91,7 +130,20 @@ func combatRound(deck1, deck2 *Deck, recursive bool) {
 		for _, card := range (*deck2)[:card2] {
 			topCards2.placeAtBottom(card)
 		}
-		winner, _ = combatGame(topCards1, topCards2, true)
+
+		if log {
+			fmt.Printf("Playing sub-game to determine the winner...\n\n")
+		}
+		subGameCounter := Counter{
+			round:       1,
+			game:        counter.highestGame + 1,
+			highestGame: counter.highestGame + 1,
+		}
+		counter.highestGame = subGameCounter.game
+		winner, _ = combatGame(topCards1, topCards2, true, log, subGameCounter)
+		if log {
+			fmt.Printf("...anyway, back to game %d.\n", counter.game)
+		}
 	} else if card1 > card2 { // player 1 wins round
 		winner = 1
 	} else if card2 > card1 { // player 2 wins round
@@ -107,6 +159,14 @@ func combatRound(deck1, deck2 *Deck, recursive bool) {
 		deck2.placeAtBottom(card1)
 	} else {
 		panic(fmt.Errorf("game is over without a winner"))
+	}
+
+	if log {
+		if recursive {
+			fmt.Printf("Player %d wins round %d of game %d!\n\n", winner, counter.round, counter.game)
+		} else {
+			fmt.Printf("Player %d wins the round!\n\n", winner)
+		}
 	}
 }
 
