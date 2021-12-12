7cc7a966b8a0cd531510e998fb31099412e1d6d8
Cargonat <c4rgon4t@gmail.com>
2021-12-12 13:01:26 +0100

    day 03 part 2

--- 2021/day01.clj
+++ 2021/day01.clj
@@ -1,12 +1,23 @@
 (require '[clojure.string])
 
+; parse input file to integer vectors
 (defn- get-input  []
-  (let [file (slurp "day01.txt")
-        lines (clojure.string/split file #"\r|\n|\r\n")
-        non-empty-lines (filter #(not= "" %) lines)]
+  (let [
+        ; read file
+        file (slurp "day01.txt") 
+
+        ; split lines
+        lines (clojure.string/split file #"\r|\n|\r\n") 
+
+        ; remove empty lines
+        non-empty-lines (filter #(not= "" %) lines)] 
+
+    ; parse to integers
     (for [s non-empty-lines] (Integer/parseInt s))))
 
+; count the number of height increases
 (defn how-many-increases [heights]
+  ; reduction with pair of number of increases and previous value
   (get (reduce (fn [[sum prev] curr]
                  [(if (and (not= prev nil)
                            (> curr prev))
@@ -15,6 +26,7 @@
                   curr])
                [0 nil] heights) 0))
 
+; add all subvectors of length n and get resulting vector
 (defn- add-tuples [input n]
   (let [in-count (count input)
         out-count (- in-count n -1)]
--- 2021/day03.clj
+++ 2021/day03.clj
@@ -6,9 +6,11 @@
     lines))
 
 (defn- lines-to-vectors [lines]
-  (for [line lines]
-    (for [char (clojure.string/split line #"")]
-      (Integer/parseInt char 2))))
+  (apply vector
+         (for [line lines]
+           (apply vector
+                  (for [char (clojure.string/split line #"")]
+                    (Integer/parseInt char 2))))))
 
 (defn- vector-sum [vectors]
   (apply (partial mapv +) vectors))
@@ -17,20 +19,41 @@
   (Integer/parseInt (clojure.string/join "" v) 2))
 
 (defn gamma-rate [vectors]
-  (let [expected (/ (count vectors) 2)
+  (let [half (/ (count vectors) 2)
         vector_sum (vector-sum vectors)
         gamma-rate-vector (for [entry vector_sum]
-                            (if (> entry expected) 1 0))]
+                            (if (> entry half) 1 0))]
     (binary-vector-to-number gamma-rate-vector)))
 
 (defn epsilon-rate [gamma-rate number-of-bits]
   (- (Math/pow 2 number-of-bits) 1 gamma-rate))
 
+(defn- get-common-bit [vectors bit common]
+  (let [half (/ (count vectors) 2)
+        found (apply + (for [v vectors] (get v bit)))
+        most-common (cond (> found half) 1 
+                          (< found half) 0
+                          :else 1 )]
+    (case common
+      :most most-common
+      :least (- 1 most-common))))
+
+(defn- filter-on-bit [vectors bit common]
+  (let [wanted (get-common-bit vectors bit common)]
+    (filter #(= wanted (get % bit)) vectors)))
+
+(defn- find-rating [vectors rating]
+  (let [common (case rating :oxygen :most :co2 :least)]
+    (loop [bit 0 vectors vectors]
+      (if (= 1 (count vectors))
+        (binary-vector-to-number (first vectors))
+        (recur (inc bit) (filter-on-bit vectors bit common))))))
+
 (let [lines (get-input-lines)
       vectors (lines-to-vectors lines)
       gamma-rate (gamma-rate vectors)
-      epsilon-rate (epsilon-rate gamma-rate (count (first vectors)))]
+      epsilon-rate (epsilon-rate gamma-rate (count (first vectors)))
+      oxygen-rating (find-rating vectors :oxygen)
+      co2-rating (find-rating vectors :co2)]
   (print "Part 1:" (int (* gamma-rate epsilon-rate))
-         "Part 2:" nil)
-)
-
+         "Part 2:" (* oxygen-rating co2-rating)))
