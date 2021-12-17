b0347ad211184fcd09d19e65fc9d0479081fef2f
Cargonat <c4rgon4t@gmail.com>
2021-12-17 21:03:44 +0100

    day 05 part 2

--- 2021/day07.clj
+++ 2021/day07.clj
@@ -1,7 +1,7 @@
 (require '[clojure.string :as str])
 
 (def day "07")
-(def use-example true)
+(def use-example false)
 (def input-file-name (str/join ["day" day (if use-example "_example" "") ".txt"]))
 
 (defn- get-input []
@@ -17,19 +17,29 @@
 (defn- distance [x y]
   (Math/abs (- x y)))
 
-(defn- fuel [distance part] 
-  (case part 
-    1 distance 
+(defn- fuel [distance part]
+  (case part
+    1 distance
     2 (int (* distance (inc distance) (/ 2))))) ; gauß sum
 
 (defn- fuel-usage [crabs target part]
-    (apply + (for [crab crabs] (fuel (distance crab target) part))))
+  (apply + (for [crab crabs] (fuel (distance crab target) part))))
 
-(defn- best-pos [crabs part] 
-  (case part 
-    1 (median crabs)
-    2 0 ; TODO
-  ))
+(defn- mean [nums]
+  (float (/ (apply + nums) 
+            (count nums))))
+
+(defn- best-pos [crabs part]
+  (case part
+    1 (median crabs) ; minimizes number of crabs on each side
+    2 (let [mean (mean crabs) ; minimizes error (distances) on each side
+            floor (Math/floor mean) ; but we have to deal with floats
+            fuel-floor (fuel-usage crabs floor 2)
+            fuel-ceil (fuel-usage crabs (inc floor) 2)
+            ] 
+        (if (< fuel-floor fuel-ceil) floor (inc floor))
+        )  
+    ))
 
 (let [crabs (get-input)
       part1 (fuel-usage crabs (best-pos crabs 1) 1)
